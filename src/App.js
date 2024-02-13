2d55c01d8a4bf0e501fd5f68cb9eb22438bc8110
Naod <naodat.dev@gmail.com>
2024-02-13 17:05:36 +0300

    Large and Small people list implemented

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import { SplitScreen } from './SplitScreen';
+// import { SplitScreen } from './SplitScreen';
+import { LargePersonListItem } from "./people/LargePersonListItem";
+import { SmallPersonListItem } from "./people/SmallPersonListItem";
+import { RegularList } from "./RegularList";
+import { people } from "./people/people";
 
-const LeftHandComponent = ({ name }) => {
-	return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>;
-}
+//1. Split Screen implementation
+// const LeftHandComponent = ({ name }) => {
+// 	return <h1 style={{ backgroundColor: 'green' }}>{name}</h1>;
+// }
 
-const RightHandComponent = ({ message }) => {
-	return <p style={{ backgroundColor: 'red' }}>{message}!</p>;
-}
+// const RightHandComponent = ({ message }) => {
+// 	return <p style={{ backgroundColor: 'red' }}>{message}!</p>;
+// }
+
+
+
 
 function App() {
 	return (
-		<SplitScreen leftWeight={1} rightWeight={3}>
-			<LeftHandComponent name="World" />
-			<RightHandComponent message="Hello" />
-		</SplitScreen>
+    <>
+		<RegularList
+			items={people}
+			resourceName="person"
+			itemComponent={SmallPersonListItem} />
+		<RegularList
+			items={people}
+			resourceName="person"
+			itemComponent={LargePersonListItem} />
+		</>
+
+//1. Split Screen implementation
+		// <SplitScreen leftWeight={1} rightWeight={3}>
+		// 	<LeftHandComponent name="World" />
+		// 	<RightHandComponent message="Hello" />
+		// </SplitScreen>
 	);
 }
 
