b004daaabd9426e5667c882254d44de25777256d
Naod <naodat.dev@gmail.com>
2024-02-13 18:38:24 +0300

    implemented modal component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { RegularList } from "./RegularList";
 import { NumberedList } from "./NumberedList";
 import { people } from "./people/people";
 import { products} from "./products/product";
+import { Modal } from "./Modal";
 
 //1. Split Screen implementation
 // const LeftHandComponent = ({ name }) => {
@@ -22,24 +23,31 @@ import { products} from "./products/product";
 
 function App() {
 	return (
-    <>
-		<RegularList
-			items={people}
-			resourceName="person"
-			itemComponent={SmallPersonListItem} />
-		<NumberedList
-			items={people}
-			resourceName="person"
-			itemComponent={LargePersonListItem} />
-		<RegularList
-			items={products}
-			resourceName="product"
-			itemComponent={SmallProductListItem} />
-		<NumberedList
-			items={products}
-			resourceName="product"
-			itemComponent={LargeProductListItem} />
+
+		<>
+		<Modal>
+			<LargeProductListItem product={products[0]} />
+		</Modal>
 		</>
+		//2. regular and numbered list
+    // <>
+	// 	<RegularList
+	// 		items={people}
+	// 		resourceName="person"
+	// 		itemComponent={SmallPersonListItem} />
+	// 	<NumberedList
+	// 		items={people}
+	// 		resourceName="person"
+	// 		itemComponent={LargePersonListItem} />
+	// 	<RegularList
+	// 		items={products}
+	// 		resourceName="product"
+	// 		itemComponent={SmallProductListItem} />
+	// 	<NumberedList
+	// 		items={products}
+	// 		resourceName="product"
+	// 		itemComponent={LargeProductListItem} />
+	// 	</>
 
 //1. Split Screen implementation
 		// <SplitScreen leftWeight={1} rightWeight={3}>
