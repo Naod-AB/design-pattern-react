090270e7037e61247e860d6188d1e6da292295c9
Naod <naodat.dev@gmail.com>
2024-02-13 21:43:00 +0300

    implemented user loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import { CurrentUserLoader } from './CurrentUserLoader';
 import { UserInfo } from './UserInfo';
+import { UserLoader } from './UserLoader';
 
 function App() {
 	return (
+<>
+		<UserLoader userId="123">
+			<UserInfo />
+		</UserLoader>
+		<UserLoader userId="234">
+			<UserInfo />
+		</UserLoader>
+		<UserLoader userId="345">
+			<UserInfo />
+		</UserLoader>
+
 		<CurrentUserLoader>
 			<UserInfo />
 		</CurrentUserLoader>
+		</>
 	);
 }
 
