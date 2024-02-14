96366a1f4149675b4bbda6ca84a5c352ee0a9c76
Naod <naodat.dev@gmail.com>
2024-02-14 12:19:51 +0300

    Applied Uncontrolled form

--- src/App.js
+++ src/App.js
@@ -1,37 +1,41 @@
 // import { CurrentUserLoader } from './CurrentUserLoader';
-import { UserInfo } from './UserInfo';
+// import { UserInfo } from './UserInfo';
 // import { UserLoader } from './UserLoader';
 // import { ResourceLoader } from './ResourceLoader';
 // import { ProductInfo } from './ProductInfo';
-import { DataSource} from './DataSource';
-import axios from 'axios';
+// import { DataSource} from './DataSource';
+// import axios from 'axios';
+import { UncontrolledForm } from "./UncontrolledForm";
 
+// for getting data from local storage
+// const getServerData = url => async () => {
+// 	const response = await axios.get(url);
+// 	return response.data;
+// }
 
-const getServerData = url => async () => {
-	const response = await axios.get(url);
-	return response.data;
-}
+// const getLocalStorageData = key => () => {
+// 	return localStorage.getItem(key);
+// }
 
-const getLocalStorageData = key => () => {
-	return localStorage.getItem(key);
-}
-
-const Text = ({ message }) => <h1>{message}</h1>;
+// const Text = ({ message }) => <h1>{message}</h1>;
 
 
 function App() {
 	return (
+//Uncontrolled form
+<UncontrolledForm />
 
-//Getting data from data source
-		<>
-		<DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
-			<UserInfo />
-		</DataSource>
 
-		<DataSource getDataFunc={getLocalStorageData('message')} resourceName="message">
-			<Text />
-		</DataSource>
-		</>
+//3. Getting data from data source
+		// <>
+		// <DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
+		// 	<UserInfo />
+		// </DataSource>
+
+		// <DataSource getDataFunc={getLocalStorageData('message')} resourceName="message">
+		// 	<Text />
+		// </DataSource>
+		// </>
 // Generic resource loader
 
 /* <>
