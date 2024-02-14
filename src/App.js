ec189ad9d75007563fe0fe5a82fc13401f0a16db
Naod <naodat.dev@gmail.com>
2024-02-14 09:12:49 +0300

    implemented loading data from source

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 // import { CurrentUserLoader } from './CurrentUserLoader';
 import { UserInfo } from './UserInfo';
 // import { UserLoader } from './UserLoader';
-import { ResourceLoader } from './ResourceLoader';
-import { ProductInfo } from './ProductInfo';
+// import { ResourceLoader } from './ResourceLoader';
+// import { ProductInfo } from './ProductInfo';
+import { DataSource} from './DataSource';
+import axios from 'axios';
+
+
+const getServerData = url => async () => {
+	const response = await axios.get(url);
+	return response.data;
+}
+
+
+
 function App() {
 	return (
 
+//Getting data from data source
+		<>
+		<DataSource getDataFunc={getServerData('/users/123')} resourceName="user">
+			<UserInfo />
+		</DataSource>
+		</>
 // Generic resource loader
 
-<>
+/* <>
 		<ResourceLoader resourceUrl="/users/123" resourceName="user">
 			<UserInfo />
 		</ResourceLoader>
 		<ResourceLoader resourceUrl="/products/1234" resourceName="product">
 			<ProductInfo />
 		</ResourceLoader>
-		</>
+		</> */
 
 // Loading current user
 /* <>
