2d9bb822b2c30bfe984e93ea9e464c39c57a0cb3
Naod <naodat.dev@gmail.com>
2024-02-14 08:52:49 +0300

    Generic resource loader implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import { CurrentUserLoader } from './CurrentUserLoader';
+// import { CurrentUserLoader } from './CurrentUserLoader';
 import { UserInfo } from './UserInfo';
-import { UserLoader } from './UserLoader';
-
+// import { UserLoader } from './UserLoader';
+import { ResourceLoader } from './ResourceLoader';
+import { ProductInfo } from './ProductInfo';
 function App() {
 	return (
+
+// Generic resource loader
+
 <>
+		<ResourceLoader resourceUrl="/users/123" resourceName="user">
+			<UserInfo />
+		</ResourceLoader>
+		<ResourceLoader resourceUrl="/products/1234" resourceName="product">
+			<ProductInfo />
+		</ResourceLoader>
+		</>
+
+// Loading current user
+/* <>
 		<UserLoader userId="123">
 			<UserInfo />
 		</UserLoader>
@@ -15,10 +29,11 @@ function App() {
 			<UserInfo />
 		</UserLoader>
 
+		current user loader
 		<CurrentUserLoader>
 			<UserInfo />
 		</CurrentUserLoader>
-		</>
+		</>*/
 	);
 }
 
